4b52dee5660b6d5c5dded417fd695d4334d45fa5
Andre Wicaksono <andrewicaksana@gmail.com>
2023-11-01 18:45:20 +0700

    #2 Setup Data Fetching Library: @tanstack/react-query (#4)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import { FC, ReactNode } from "react";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import CSSGlobal from "./global.css";
+
+const queryClient = new QueryClient();
 
 const App: FC<{ children: ReactNode }> = ({ children }) => {
   return (
-    <div className="App">
-      <CSSGlobal />
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <CSSGlobal />
 
-      {children}
-    </div>
+        {children}
+      </div>
+
+      <ReactQueryDevtools buttonPosition="bottom-left" initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
